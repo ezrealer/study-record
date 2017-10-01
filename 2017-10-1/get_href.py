6916ffa25384c6ef977ba5d1f60a1ed0623b0a79
ezrealer <ezreal_pzx@sina.com>
2017-10-01 22:47:28 +0800

    获取链接源代码

---
+++ 2017-10-1/get_href.py
@@ -0,0 +1,11 @@
+import re
+
+old_url = 'http://www.jikexueyuan.com/course/android/?pageNum=2'#假设的网址
+total_page = 20 #假设网页有20页
+
+f = open('../txt/text.txt','r')
+html = f.read()
+f.close()
+#寻找链接
+href = re.findall('href="(.*?)"',html,re.S)
+print(href)
